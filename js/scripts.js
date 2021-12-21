090b6e3a05af35e2532bccaf8f8e52ad2d97bce4
akhilpavansai <akhilpavansai.machavaram@zemosolabs.com>
2021-12-21 18:25:48 +0530

    assignemtn point1 completed

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function validate()
+{
+    console.log("hi");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if(name.trim().length===0)
+    {
+        alert("name cannot be empty");
+        return false;
+    }
+    if(email.trim().length===0)
+    {
+        alert("email cannot be empty");
+        return false;
+    }
+    if(! /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+    {
+        alert("email is not valid be empty");
+        return false;
+    }
+    return true;
+}
+function accepted()
+{
+    alert("successfully submitted the form!!");
+    return true;
+}
+
+function denied()
+{
+    alert("check your form fields, form not submitted!");
+    return false;
+}
